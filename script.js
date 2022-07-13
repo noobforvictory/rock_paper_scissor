fb403214d1111b1db8b877cf6d68bb650fb980bd
denis <denisvictorjosphine1996@gmail.com>
2022-07-13 09:10:30 +0530

    add buttons

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function computerPlay(){
         result= "paper";
         return result;
     }
-    else if(num<=0.999){
+    else if(num<0.999){
         result= "scissors";
         return result;
     }
@@ -47,19 +47,37 @@ function game(){
     let lose=0;
     let draw=0;
     let result="";
-    
-    for(let i=0; i<5; i++){
-    result= playRound();
-    if(result==="w"){
-        win++;
-    }
-    else if(result==="l"){
-        lose++;
-    }
-    else {
-        draw++;
-    }
-    }
-    console.log("you won "+win+" times. you lost "+lose+" times. game draw "+draw+" times.")
+    playRound();
+    // for(let i=0; i<5; i++){
+    // result= playRound();
+    // if(result==="w"){
+    //     win++;
+    // }
+    // else if(result==="l"){
+    //     lose++;
+    // }
+    // else {
+    //     draw++;
+    // }
+    // }
+    // console.log("you won "+win+" times. you lost "+lose+" times. game draw "+draw+" times.")
 }
-game();
+
+
+const button1 = document.createElement('button');
+button1.textContent="rock";
+button1.classList.add('button');
+const button2 = document.createElement('button');
+button2.textContent="paper";
+button2.classList.add('button');
+const button3 = document.createElement('button');
+button3.textContent="scissor";
+button3.classList.add('button');
+const body = document.querySelector('.body');
+body.appendChild(button1);
+body.appendChild(button2);
+body.appendChild(button3);
+
+    
+
+
